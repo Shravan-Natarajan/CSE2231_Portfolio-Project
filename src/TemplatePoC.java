e6430e686856aa6b1a81e85bba1830fff98bae67
Shravan-Natarajan <shravan.nataraj@gmail.com>
2024-11-17 13:24:57 -0500

    the IDE just sucks but it works i swear

--- src/TemplatePoC.java
+++ src/TemplatePoC.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 
+import components.map.Map1L;
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
@@ -7,44 +8,66 @@ import components.simplewriter.SimpleWriter1L;
 
 public class TemplatePoC {
 
-    private String templateString;
+    //private String templateString;
 
-    public TemplatePoC(String templateString) {
-        this.templateString = templateString;
-        // Hi $name, welcome to $site
-    }
+    //public TemplatePoC(String templateString) {
+    //this.templateString = templateString;
+    // Hi $name, welcome to $site
+    //}
 
     //public String substitute(Map<String, String> mapping) {
     // {("name": "Joe"), ("site", "google.com")}
     //this.templateString.split(this.templateString);
     //}
 
-    public Map<String, String> substitute(String key, String value) {
-        Map<String, String> mapping = new Map1L<>();
-        Map.add(key, value);
-        return mapping;
+    //public Map<String, String> substitute(String key, String value) {
+    //Map<String, String> mapping = new Map1L<>();
+    //mapping.add(key, value);
+    //return mapping;
 
-    }
+    //}
 
-    public void printToFile(SimpleWriter outputLocale,
-            Map<String, String> mapping) {
-        outputLocale.println("Hi, my name is " + map.key() + "Im in your "
-                + map.value() + "class.");
+    /*
+     *
+     */
+    public static void printToFile(SimpleWriter outputLocale,
+            Map<String, String> mapping, int numOfClasses) {
+        Map<String, String> mapCopy = mapping.newInstance();
+        for (int i = 0; i < numOfClasses; i++) {
+            Map.Pair<String, String> nameAndTime = mapping.removeAny();
+            outputLocale.println("Hi, my name is " + nameAndTime.key()
+                    + "Im in your " + nameAndTime.value() + "class.");
+            mapCopy.add(nameAndTime.key(), nameAndTime.value());
+        }
+        mapping.transferFrom(mapCopy);
     }
 
     public static void main(String[] args) {
         SimpleWriter out = new SimpleWriter1L();
         SimpleReader in = new SimpleReader1L();
-        System.out.print("Provide file: ");
-        String outputFile = out.println();
-        SimpleWriter outputFileLocation = new SimpleWriter1L(outputFile); //Add file output here.
-        System.out.print("Provide name: ");
-        String key = in.println();
-        System.out.print("Provide time: ");
-        String value = in.println();
-        // get data
-        Map<String, String> mapping = this.substitute(key, value);
-        this.printToFile(outputFileLocation, mapping);
+
+        //Add file output location here.
+        out.println("Provide file locale: ");
+        String outputFile = in.nextLine();
+        //Initialize output to file
+        SimpleWriter outputFileLocation = new SimpleWriter1L(outputFile);
+
+        //Initializes # of loops
+        out.println("How many classes will you be adding?");
+        String numOfClassesString = in.nextLine();
+        int numOfClassesFR = Integer.parseInt(numOfClassesString);
+
+        //Collect data
+        Map<String, String> mapping = new Map1L();
+        for (int i = 0; i < numOfClassesFR; i++) {
+            out.println("Provide name/nickname for class " + i + ":");
+            String key = in.nextLine();
+            out.println("Provide time for class " + i + " :");
+            String value = in.nextLine();
+            mapping.add(key, value);
+        }
+        //Print data
+        printToFile(outputFileLocation, mapping, numOfClassesFR);
     }
 
 }
